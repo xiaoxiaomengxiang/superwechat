f251b91bccea39e43c2784a711002b4c98e6eeb1
wenxiaobo <279918814@qq.com>
2016-11-01 19:25:38 +0800

    添加工具类

--- ChatDemoUI3.0/src/cn/yp/superwechat/ui/GuideActivity.java
+++ ChatDemoUI3.0/src/cn/yp/superwechat/ui/GuideActivity.java
@@ -1,8 +1,12 @@
 package cn.yp.superwechat.ui;
 
 import android.os.Bundle;
+import android.view.View;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.yp.superwechat.R;
+import cn.yp.superwechat.utils.MFGT;
 
 
 public class GuideActivity extends BaseActivity {
@@ -10,6 +14,19 @@ public class GuideActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_guide);
+        ButterKnife.bind(this);
         super.onCreate(savedInstanceState);
+    }
+
+    @OnClick({R.id.but_register, R.id.but_login})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.but_register:
+                MFGT.gotoRegister(this);
+                break;
+            case R.id.but_login:
+                MFGT.gotoLogin(this);
+                break;
+        }
     }
 }
