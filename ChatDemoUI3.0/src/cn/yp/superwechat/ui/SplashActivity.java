5dbc8738842876ea1ade9e8525f133ed63dd8687
wenxiaobo <279918814@qq.com>
2016-11-03 23:33:18 +0800

    登录成功闪屏获取数据保存到内存中

--- ChatDemoUI3.0/src/cn/yp/superwechat/ui/SplashActivity.java
+++ ChatDemoUI3.0/src/cn/yp/superwechat/ui/SplashActivity.java
@@ -4,22 +4,29 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.easeui.domain.User;
 
-import cn.yp.superwechat.SuperWeChatHelper;
 import cn.yp.superwechat.R;
+import cn.yp.superwechat.SuperWeChatHelper;
+import cn.yp.superwechat.db.UserDao;
+import cn.yp.superwechat.utils.L;
 
 /**
  * 开屏页
  *
  */
+
 public class SplashActivity extends BaseActivity {
+	private static final String TAG = SplashActivity.class.getSimpleName();
 
 	private static final int sleepTime = 2000;
+	SplashActivity mContext;
 
 	@Override
 	protected void onCreate(Bundle arg0) {
 		setContentView(R.layout.em_activity_splash);
 		super.onCreate(arg0);
+		mContext = this;
 	}
 
 	@Override
@@ -33,6 +40,10 @@ public class SplashActivity extends BaseActivity {
 					long start = System.currentTimeMillis();
 					EMClient.getInstance().groupManager().loadAllGroups();
 					EMClient.getInstance().chatManager().loadAllConversations();
+					UserDao dao = new UserDao(mContext);
+					User user = dao.getUser(EMClient.getInstance().getCurrentUser());
+					L.e(TAG,"user="+user);
+					SuperWeChatHelper.getInstance().setCurrentUser(user);
 					long costTime = System.currentTimeMillis() - start;
 					//wait
 					if (sleepTime - costTime > 0) {
@@ -57,11 +68,64 @@ public class SplashActivity extends BaseActivity {
 		}).start();
 
 	}
-	
+
 	/**
 	 * get sdk version
 	 */
 	private String getVersion() {
-	    return EMClient.getInstance().getChatConfig().getVersion();
+		return EMClient.getInstance().getChatConfig().getVersion();
 	}
 }
+//public class SplashActivity extends BaseActivity {
+//
+//	private static final int sleepTime = 2000;
+//
+//	@Override
+//	protected void onCreate(Bundle arg0) {
+//		setContentView(R.layout.em_activity_splash);
+//		super.onCreate(arg0);
+//	}
+//
+//	@Override
+//	protected void onStart() {
+//		super.onStart();
+//
+//		new Thread(new Runnable() {
+//			public void run() {
+//				if (SuperWeChatHelper.getInstance().isLoggedIn()) {
+//					// auto login mode, make sure all group and conversation is loaed before enter the main screen
+//					long start = System.currentTimeMillis();
+//					EMClient.getInstance().groupManager().loadAllGroups();
+//					EMClient.getInstance().chatManager().loadAllConversations();
+//					long costTime = System.currentTimeMillis() - start;
+//					//wait
+//					if (sleepTime - costTime > 0) {
+//						try {
+//							Thread.sleep(sleepTime - costTime);
+//						} catch (InterruptedException e) {
+//							e.printStackTrace();
+//						}
+//					}
+//					//enter main screen
+//					startActivity(new Intent(SplashActivity.this, MainActivity.class));
+//					finish();
+//				}else {
+//					try {
+//						Thread.sleep(sleepTime);
+//					} catch (InterruptedException e) {
+//					}
+//					startActivity(new Intent(SplashActivity.this, GuideActivity.class));
+//					finish();
+//				}
+//			}
+//		}).start();
+//
+//	}
+//
+//	/**
+//	 * get sdk version
+//	 */
+//	private String getVersion() {
+//	    return EMClient.getInstance().getChatConfig().getVersion();
+//	}
+//}
