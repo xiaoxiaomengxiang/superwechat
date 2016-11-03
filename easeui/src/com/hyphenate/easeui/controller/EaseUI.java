4e052f80e36f9841cd4787c836de95bdf30bd1b1
wenxiaobo <279918814@qq.com>
2016-11-03 23:42:49 +0800

    设置User数据的Provider

--- easeui/src/com/hyphenate/easeui/controller/EaseUI.java
+++ easeui/src/com/hyphenate/easeui/controller/EaseUI.java
@@ -1,9 +1,10 @@
 package com.hyphenate.easeui.controller;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.util.Log;
 
 import com.hyphenate.EMMessageListener;
 import com.hyphenate.chat.EMClient;
@@ -11,14 +12,14 @@ import com.hyphenate.chat.EMMessage;
 import com.hyphenate.chat.EMOptions;
 import com.hyphenate.easeui.domain.EaseEmojicon;
 import com.hyphenate.easeui.domain.EaseUser;
+import com.hyphenate.easeui.domain.User;
 import com.hyphenate.easeui.model.EaseAtMessageHelper;
 import com.hyphenate.easeui.model.EaseNotifier;
 
-import android.app.Activity;
-import android.app.ActivityManager;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.util.Log;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public final class EaseUI {
     private static final String TAG = EaseUI.class.getSimpleName();
@@ -246,6 +247,7 @@ public final class EaseUI {
          * @return
          */
         EaseUser getUser(String username);
+        User getAppUser(String username);
     }
     
     /**
